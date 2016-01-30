477184ba78dabef4562e4dd8e2393ce351d29153
Daniel Savage <dansvg@gmail.com>
2016-01-31 03:06:57 +0900

    Player join lobby

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ var main = function() {
   var game = new Phaser.Game(GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT + UI_BAR_HEIGHT);
 
   game.state.add('Preload', Preload, false);
+  game.state.add('TitleScreen', TitleScreen, false);
   game.state.add('Gameplay', Gameplay, false);
 
   game.state.start('Preload');
---
+++ src/titlescreen.js
@@ -0,0 +1,98 @@
+var TitleScreen = function(game) { };
+TitleScreen.prototype.init = function() {
+  this.enteredPlayers = [];
+};
+TitleScreen.prototype.create = function() {
+  this.debugSlotsText = this.game.add.text(160, 160, '(no players)', { fill: 'white', font: '64px Arial'});
+
+  var spaceKey = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+  spaceKey.onUp.add(function () { this.pushInput(this.game.input.keyboard); }, this);
+
+  var backspaceKey = this.game.input.keyboard.addKey(Phaser.KeyCode.BACKSPACE);
+  backspaceKey.onUp.add(function () { this.removeInput(this.game.input.keyboard); }, this);
+
+  var enterKey = this.game.input.keyboard.addKey(Phaser.KeyCode.ENTER);
+  enterKey.onUp.add(function () {
+    this.startWithInput(this.game.input.keyboard);
+  }, this);
+
+  // start Gameplay state with something like[this.game.input.gamepad.pad1, this.game.input.keyboard, this.game.input.gamepad.pad2]
+};
+TitleScreen.prototype.update = function() {
+  if (this.game.input.gamepad.pad1.justPressed(Phaser.Gamepad.XBOX360_A)) {
+    this.pushInput(this.game.input.gamepad.pad1);
+  }
+  if (this.game.input.gamepad.pad2.justPressed(Phaser.Gamepad.XBOX360_A)) {
+    this.pushInput(this.game.input.gamepad.pad2);
+  }
+  if (this.game.input.gamepad.pad3.justPressed(Phaser.Gamepad.XBOX360_A)) {
+    this.pushInput(pthis.game.input.gamepad.pad3);
+  }
+  if (this.game.input.gamepad.pad4.justPressed(Phaser.Gamepad.XBOX360_A)) {
+    this.pushInput(this.game.input.gamepad.pad4);
+  }
+
+  if (this.game.input.gamepad.pad1.justPressed(Phaser.Gamepad.XBOX360_B)) {
+    this.removeInput(this.game.input.gamepad.pad1);
+  }
+  if (this.game.input.gamepad.pad2.justPressed(Phaser.Gamepad.XBOX360_B)) {
+    this.removeInput(this.game.input.gamepad.pad2);
+  }
+  if (this.game.input.gamepad.pad3.justPressed(Phaser.Gamepad.XBOX360_B)) {
+    this.removeInput(this.game.input.gamepad.pad3);
+  }
+  if (this.game.input.gamepad.pad4.justPressed(Phaser.Gamepad.XBOX360_B)) {
+    this.removeInput(this.game.input.gamepad.pad4);
+  }
+
+  if (this.game.input.gamepad.pad1.justPressed(Phaser.Gamepad.XBOX360_START)) {
+    this.startWithInput(this.game.input.gamepad.pad1);
+  }
+  if (this.game.input.gamepad.pad2.justPressed(Phaser.Gamepad.XBOX360_START)) {
+    this.startWithInput(this.game.input.gamepad.pad2);
+  }
+  if (this.game.input.gamepad.pad3.justPressed(Phaser.Gamepad.XBOX360_START)) {
+    this.startWithInput(this.game.input.gamepad.pad3);
+  }
+  if (this.game.input.gamepad.pad4.justPressed(Phaser.Gamepad.XBOX360_START)) {
+    this.startWithInput(this.game.input.gamepad.pad4);
+  }
+};
+
+TitleScreen.prototype.pushInput = function(input) {
+  if (this.enteredPlayers.indexOf(input) !== -1)
+  {
+    return;
+  }
+
+  this.enteredPlayers.push(input);
+
+  this.debugSlotsText.text = '';
+  this.enteredPlayers.forEach(function (player) { this.debugSlotsText.text += player + '\n'; }, this);
+};
+TitleScreen.prototype.removeInput = function(input) {
+  if (this.enteredPlayers.indexOf(input) === -1)
+  {
+    return;
+  }
+
+  this.enteredPlayers.splice(this.enteredPlayers.indexOf(input), 1);
+
+  if (this.enteredPlayers.length === 0)
+  {
+    this.debugSlotsText.text = '(no players)';
+  }
+  else
+  {
+    this.debugSlotsText.text = '';
+    this.enteredPlayers.forEach(function (player) { this.debugSlotsText.text += player + '\n'; }, this);
+  }
+};
+TitleScreen.prototype.startWithInput = function(input) {
+  if (this.enteredPlayers.length < 2 || this.enteredPlayers.indexOf(input) === -1)
+  {
+    return;
+  }
+
+  this.game.state.start('Gameplay', true, false, this.enteredPlayers);
+};
