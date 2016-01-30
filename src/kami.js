2633b75aa02ead1118eef5f4883da9d1133d53fd
Daniel Savage <dansvg@gmail.com>
2016-01-31 00:18:45 +0900

    Kami randomly crawl the area

--- src/kami.js
+++ src/kami.js
@@ -1,3 +1,5 @@
+var KAMI_MOVE_SPEED = 50;
+
 var Kami = function (game, x, y, index) {
   Phaser.Sprite.call(this, game, x, y, 'tiles_s', 0);
   this.anchor.set(0.5, 0.5);
@@ -5,7 +7,7 @@ var Kami = function (game, x, y, index) {
   this.game.physics.arcade.enable(this);
   this.body.setSize(32, 32);
 
-  this.tint = 0xFF0000;
+  this.target = new Phaser.Point(GAME_SCREEN_WIDTH / 2, GAME_SCREEN_HEIGHT / 2);
 
   this.index = index;
 
@@ -15,5 +17,11 @@ Kami.prototype = Object.create(Phaser.Sprite.prototype);
 Kami.prototype.constructor = Kami;
 
 Kami.prototype.update = function () {
-  this.body.velocity.set(Math.random() * 100 - 50, Math.random() * 100 - 50);
+  var deltaDirection = Phaser.Point.normalize(Phaser.Point.subtract(this.target, this.position));
+  this.body.velocity.set(deltaDirection.x * KAMI_MOVE_SPEED, deltaDirection.y * KAMI_MOVE_SPEED);
+
+  if (Math.sqrt((this.target.x - this.x) * (this.target.x - this.x) + (this.target.y - this.y) * (this.target.y - this.y)) < 16) {
+    this.target.x = GAME_SCREEN_WIDTH * Math.random() * 0.8 + (0.1);
+    this.target.y = GAME_SCREEN_HEIGHT * Math.random() * 0.8 + (0.1);
+  }
 };
