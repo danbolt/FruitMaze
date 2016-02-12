50aa5902909dac720778577d8f072260ac6c026b
Daniel Savage <dansvg@gmail.com>
2016-02-12 10:49:54 +0900

    Create inputHandler.js

---
+++ src/inputHandler.js
@@ -0,0 +1,33 @@
+var InputHandler = function(game, device, up, down, left, right, accpet, back) {
+  if (device instanceof Phaser.SinglePad || device instanceof Phaser.Keyboard) {
+    this.device = device;
+  }
+  else {
+    throw "No proper device specified";
+  }
+
+  this.upButton = up;
+  this.downButton = down;
+  this.leftButton = left;
+  this.rightButton = right;
+  this.acceptButton = accept;
+  this.backButton = back;
+};
+InputHandler.prototype.isDown = function (value) {
+  switch (value) {
+    case 'up':
+      return this.device.isDown(this.upButton);
+    case 'down':
+      return this.device.isDown(this.downButton);
+    case 'left':
+      return this.device.isDown(this.leftButton);
+    case 'right':
+      return this.device.isDown(this.rightButton);
+    case 'accept':
+      return this.device.isDown(this.acceptButton);
+    case 'back':
+      return this.device.isDown(this.backButton);
+    default:
+      return false;
+  }
+};
